52b5c68b770f766ad0b452d34315c442aa04b84b
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-07 12:53:44 -0800

    added button functionality

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
+var mario = new Tamagotchi('Mario', 'monkey');
+
 function Tamagotchi(name, kind) {
     var that = this;
     this.name = name;
     this.kind = kind;
     this.health = 100;
     this.feed = function() {
-        that.health += 5;
+        that.health += 10;
+        updateScore()        
+    }
+    this.pet = function() {
+        that.health += 1;
+        updateScore()        
     }
     this.start = function() {
         that.interval = setInterval(function() {
             that.health -= 5;
-            console.log(that.health);
+            updateScore()
             if (that.health <=0) {
                 clearInterval(that.interval);
-                console.log(that.name + ' died');
+                $('#health').text('GAME OVER')
             }
         }, 1000);
     }
 }
+
+function updateScore() {
+    $('#health').text('Health: ' + mario.health);
+}
+
+$('#start').click(function() {
+    mario.start();
+});
+
+$('#feed').click(function() {
+    mario.feed();
+});
+
+$('#pet').click(function() {
+    mario.pet();
+});
